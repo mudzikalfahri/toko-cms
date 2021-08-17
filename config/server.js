293bb753432c061248b00e9cfb2353c054d2af17
unknown <alfahrimudzik@gmail.com>
2021-08-17 11:19:07 +0700

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8431c625cac94e7ef8143372f074f507'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c33525f3-3174-44e1-a855-fd0f348fc13f'
+};
